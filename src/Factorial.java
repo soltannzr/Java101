37c5b044cf5635ee9f2c84a9a9acbdf7e5999aa1
Soltan Nazarov <sltn.soft@gmail.com>
2023-05-19 05:02:50 +0300

    First Version

---
+++ src/Factorial.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner (System.in);
+        int n,r,nFac =1 , rFac = 1, nrFac = 1, combineson;
+
+        System.out.println("Enter the numbers you want to calculate the combination according to C(n,r)" );
+        System.out.print("Enter n: ");
+        n = inp.nextInt();
+        System.out.print("Enter r: ");
+        r = inp.nextInt();
+
+        for (int i = 1;i<=n;i++){
+            nFac *= i;
+        }
+        for (int j = 1;j<=r;j++){
+            rFac *= j;
+        }
+        for (int k = 1;k <=(n-r);k++){
+            nrFac *= k;
+        }
+        combineson = nFac/(rFac * nrFac);
+        System.out.println("C(" + n + "," + r +") : " + combineson);
+    }
+}
