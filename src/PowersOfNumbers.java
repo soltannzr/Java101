6667cb9718dc208238cc45949b42452d82cefae6
Soltan Nazarov <sltn.soft@gmail.com>
2023-05-19 04:28:44 +0300

    First Version

--- src/PowersOfNumbers.java
+++ src/PowersOfNumbers.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
-public class PowersOfTwo {
+public class PowersOfNumbers {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int a,j,i;
         System.out.print("Enter number: ");
         a = inp.nextInt();
         System.out.print("Powers of 4: ");
-        for ( i = 1;i<a; i*=4){
-            if (i*4 >=a)
+        for ( i = 1;i<a; i*=4) {
+            if (i * 4 >= a)
                 System.out.println(i);
             else
-                System.out.print( i+",");
+                System.out.print(i + ",");
         }
 
         System.out.print("Powers of 5: ");
