45a8e537f8c7f91b04743d721ed4e14aaa83f6e6
Soltan Nazarov <sltn.soft@gmail.com>
2023-05-19 05:07:15 +0300

    First Version

--- src/CombinationCalculator.java
+++ src/CombinationCalculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class CombinationCalculator {
     public static void main(String[] args) {
         Scanner inp = new Scanner (System.in);
-        int n,r,nFac =1 , rFac = 1, nrFac = 1, combineson;
+        int n,r,nFac =1 , rFac = 1, nrFac = 1, combination;
 
         System.out.println("Enter the numbers you want to calculate the combination according to C(n,r)" );
         System.out.print("Enter n: ");
@@ -21,7 +21,7 @@ public class CombinationCalculator {
         for (int k = 1;k <=(n-r);k++){
             nrFac *= k;
         }
-        combineson = nFac/(rFac * nrFac);
-        System.out.println("C(" + n + "," + r +") : " + combineson);
+        combination = nFac/(rFac * nrFac);
+        System.out.println("C(" + n + "," + r +") : " + combination);
     }
 }
